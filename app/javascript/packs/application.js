393ba36b9fd37080123cb28db1dc3bac096ee10a
jake <jake@mythical.fish>
2017-11-24 11:33:09 +0100

    add turbolinks support

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,42 +1,17 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you set extractStyles to true
-// in config/webpack/loaders/vue.js) to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
 import Vue from 'vue';
+import TurbolinksAdapter from 'vue-turbolinks';
 import App from '../components/app.vue';
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('hello'));
-  const app = new Vue(App).$mount('hello');
+Vue.use(TurbolinksAdapter)
 
-  console.log(app);
+document.addEventListener('turbolinks:load', () => {
+  var element = document.getElementById("writer")
+  if (element != null) {
+    const app = new Vue(App).$mount(element);
+    /*var vueapp = new Vue({
+      el: element,
+      template: '<App/>',
+      components: { App }
+    });*/
+  }
 });
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-// import Vue from 'vue/dist/vue.esm'
-// import App from './app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
