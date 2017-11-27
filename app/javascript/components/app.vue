b91ad9faaca10b76dcc4a046b9a8129970d378eb
jake <jake@mythical.fish>
2017-11-27 14:31:46 +0100

    add element-ui, start adding create form

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <el-button type="primary" v-on:click='isCreating = !isCreating'>New document</el-button>
+    <div v-show="isCreating">
+      <el-form ref="form" :model="form">
+        <el-form-item label="Document name">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Hello Vue!"
+      message: 'Hello Vue!',
+      isCreating: false,
+      form: {
+        name: ''
+      }
     }
   }
 }
--- app/javascript/packs/documents.js
+++ app/javascript/packs/documents.js
@@ -1,15 +1,13 @@
-import Vue from 'vue';
-import TurbolinksAdapter from 'vue-turbolinks';
-import App from '../components/app.vue';
+import Vue from 'vue'
+import TurbolinksAdapter from 'vue-turbolinks'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from '../components/app.vue'
 
+Vue.use(ElementUI)
 Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
-  var element = document.getElementById("documents")
-  const app = new Vue(App).$mount(element);
-  /*var vueapp = new Vue({
-    el: element,
-    template: '<App/>',
-    components: { App }
-  });*/
-});
+  var element = document.getElementById('documents')
+  new Vue(App).$mount(element)
+})
