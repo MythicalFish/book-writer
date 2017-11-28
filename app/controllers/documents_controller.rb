c3538d77190b4903964ee7084135c76921535787
jake <jake@mythical.fish>
2017-11-28 12:58:52 +0100

    add some notes

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,9 +1,18 @@
 class DocumentsController < ApplicationController
+
+  # This app is partly standard Rails, and partly an SPA.
+  # The index & edit actions do load standard HTML views, but those
+  # only serve to provide the bookmarkable URLs, and the Vue JS, which
+  # then calls this controller again to do the actual work.
+
+  # I could have used Vue Router to achieve the same thing, but this 
+  # is quicker & easier, and is one of the advantages provided by
+  # using Rails & Webpacker over a Rails API with a seperate frontend.
   
   def index
     @documents = current_user.documents 
     respond_to do |format|
-      format.html # Loads the pack which then requests the JSON
+      format.html 
       format.json { render json: @documents }
     end
   end
